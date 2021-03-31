398eeaaad4cf94d0dcf96280ff369fb439664d25
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-31 09:40:27 -0500

    commit tree 1d263c1d7f3eb7832d0002344ef0f555ddb88971

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+
+class Polygon {
+    constructor(array) {
+        this.array = array
+    }
+
+    get countSides() {
+        return this.array.length
+    }
+    get perimeter() {
+        return this.array.reduce((total, current) => total + current)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.countSides === 3) {
+            let a = this.array[0]
+            let b = this.array[1]
+            let c = this.array[2]
+
+            if (a + b >= c && a + c >= b && b + c >= a) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        if (this.countSides === 4) {
+            let a = this.array[0]
+            let b = this.array[1]
+            let c = this.array[2]
+            let d = this.array[3]
+
+            if (a === b && b === c && c === d) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+
+    get area() {
+        if (this.isValid) {
+            return this.array[0] ** 2
+        }
+    }
+}
